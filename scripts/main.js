1afdcf7fa2976f673dfa78f4404f2d8c125fcfdc
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-12 10:36:07 -0500

    final code

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+fetch('https://randomfox.ca/floof')
+    .then(response => response.json())
+    .then(
+        (convertedData) => {
+            const foxImgElement = document.querySelector("#fox")
+            foxImgElement.src = convertedData.image
+        }
+    )
+
+const getTheCat = async () => {
+    const response = await fetch('http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false')
+    const convertedData = await response.json()
+
+    const catImgElement = document.querySelector("#cat")
+    catImgElement.src = convertedData[0]
+}
+
+getTheCat()
